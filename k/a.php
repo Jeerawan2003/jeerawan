4e0f0bddf784de8cdf7b7c1b83eed3dfe42ff0f0
jeerawan <66010914136@msu.ac.th>
2026-02-18 17:14:31 +0700

    a

--- k/a.php
+++ k/a.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 <h1>งาน k 66010914136 จีรวรรณ์ มาทอ </h1>
-<button style="backrround-color: green;"><a href ='1.jpg'>รูปตัวเอง</a></button>
-<button style="backrround-color: green;"><a href ='2.jpg'>รูปอาจจารย์</a></button>
+<button style="background-color: green;"><a href ='1.jpg'>รูปตัวเอง</a></button>
+<button style="background-color: yellow;"><a href ='2.jpg'>รูปอาจจารย์</a></button>
 
 </body>
 </html>
